315fc67b2dafd26658e63855417905cbc44417b4
unknown <ta.shaikat@gmail.com>
2020-05-14 15:41:43 +0600

    navbar

--- js/new-layout.js
+++ js/new-layout.js
@@ -6,3 +6,11 @@
 //     $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 // });
 
+$(document).ready(function(){
+   $('.dropdown-submenu a.test').on("click", function(e){
+     $(this).next('ul').toggle();
+     e.stopPropagation();
+     e.preventDefault();
+   });
+ });
+
